7789788dd911da05e514ad3d188b84306c099763
Satriawan Basri <satriawan.basri@gmail.com>
2023-02-21 09:34:19 +0700

    init src

---
+++ Test_technical_code/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Test_technical_code.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
