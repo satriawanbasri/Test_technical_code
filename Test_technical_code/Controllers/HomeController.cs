a735396037a9e96d6dd878441794516d5aa3282a
Satriawan Basri <satriawan.basri@gmail.com>
2023-02-21 10:19:36 +0700

    saving progress

--- Test_technical_code/Controllers/HomeController.cs
+++ Test_technical_code/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
 using System.Diagnostics;
 using Test_technical_code.Models;
 
@@ -29,7 +30,51 @@ namespace Test_technical_code.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        public JsonResult Segitiga(int inputAngka)
+        {
+            return Json("test");
+        }
 
+        public JsonResult Ganjil(int inputAngka)
+        {
+            var listGanjil = new List<int>();
+            var ganjil = 0;
+            listGanjil.Add(ganjil);
+            var i = 1;
+            while (ganjil <= inputAngka)
+            {
+                ganjil = 2 * i - 1;
+                if (ganjil <= inputAngka)
+                    listGanjil.Add(ganjil);
+                i++;
+            }
+            return Json(listGanjil);
+        }
+
+        public JsonResult Prima(int inputAngka)
+        {
+            var listPrima = new List<int>();
+            bool prima = true;
+            int bilangan = inputAngka;
+            if (bilangan >= 2)
+            {
+                for (int i = 2; i <= bilangan; i++)
+                {
+                    for (int j = 2; j < i; j++)
+                    {
+                        if ((i % j) == 0)
+                        {
+                            prima = false;
+                            break;
+                        }
+                    }
+                    if (prima)
+                        listPrima.Add(i);
+                    prima = true;
+                }
+            }
+            return Json(listPrima);
+        }
 
     }
 }
